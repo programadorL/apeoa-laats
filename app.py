88f5953a860e596b90c87c2f40a91c20f977026d
estebancabreraa <esteban.cabrera.a@gmail.com>
2022-03-31 12:11:10 -0600

    progress & gitignore test

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, render_template
-import os
 
-directory_path = os.getcwd()
-
-app = Flask(__name__) #static_folder = directory_path)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
