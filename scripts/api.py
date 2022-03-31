2bd5c46c647db58b55f020af03ae6fef34e8186e
estebancabreraa <esteban.cabrera.a@gmail.com>
2022-03-31 15:12:43 -0600

    api: progress in user_auth

--- scripts/api.py
+++ scripts/api.py
@@ -6,10 +6,10 @@ def user_auth(email, password):
     try: 
         with pyodbc.connect('DRIVER='+DRIVER+';SERVER=tcp:'+SERVER+';PORT=1433;DATABASE='+DATABASE+';UID='+USERNAME+';PWD='+ PASSWORD) as conn:
             with conn.cursor() as cursor:
-                cursor.execute("SELECT pin FROM usuarios WHERE usuario ='" + email + "' And pin ='" + password + "';")
+                cursor.execute("SELECT usuario, pin FROM usuarios WHERE usuario ='" + email + "' And pin ='" + password + "';")
                 result = cursor.fetchall()
-                print(result)
+                if (email == result[0][0] and password == result[0][1]):
+                    return True
     except Exception as e:
         print("Ocurrió un error al conectar a SQL Server: ", e)
-
-print(user_auth('programadorit@laats.net', '7070'))
+        return False
