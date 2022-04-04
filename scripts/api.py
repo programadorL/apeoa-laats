a6b0d5b0f0aeb4115542dcdbf20f1d1ddd3279ec
estebancabreraa <esteban.cabrera.a@gmail.com>
2022-04-04 10:47:49 -0600

    get_flights progress

--- scripts/api.py
+++ scripts/api.py
@@ -2,26 +2,23 @@ from query_builder import *
 import pyodbc
 from env import DATABASE, DRIVER, PASSWORD, PORT, SERVER, USERNAME
 
-#test comment
 def user_auth(email, password):
     try: 
         with pyodbc.connect('DRIVER='+DRIVER+';SERVER=tcp:'+SERVER+';PORT='+PORT+';DATABASE='+DATABASE+';UID='+USERNAME+';PWD='+ PASSWORD) as conn:
             with conn.cursor() as cursor:
-                cursor.execute(get_user_auth_query())
+                cursor.execute(get_user_auth_query(email, password))
                 result = cursor.fetchall()
                 if (email == result[0][0] and password == result[0][1]):
                     return True
     except Exception as e:
         print("Ocurrió un error al conectar a SQL Server: ", e)
         return False
-
-print(user_auth('programadorit@laats.net', '7070'))
-
-def get_flights(date):
+        
+def get_flights(day, month, operator, departamento):
     try: 
-        with pyodbc.connect('DRIVER='+DRIVER+';SERVER=tcp:'+SERVER+';PORT=1433;DATABASE='+DATABASE+';UID='+USERNAME+';PWD='+ PASSWORD) as conn:
+        with pyodbc.connect('DRIVER='+DRIVER+';SERVER=tcp:'+SERVER+';PORT='+PORT+';DATABASE='+DATABASE+';UID='+USERNAME+';PWD='+ PASSWORD) as conn:
             with conn.cursor() as cursor:
-                cursor.execute()
+                cursor.execute(get_flights_query(day, month, operator, departamento))
                 result = cursor.fetchall()
     except Exception as e:
         print("Ocurrió un error al conectar a SQL Server: ", e)
