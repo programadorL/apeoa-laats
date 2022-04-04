7c556d4f65daa4373b1029d6cf726b914522ec33
estebancabreraa <esteban.cabrera.a@gmail.com>
2022-04-04 15:03:56 -0600

    initial work in dashboard

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('dashboard.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
