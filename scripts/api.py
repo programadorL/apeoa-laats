dd2b9304390496a5b6e875ee7e9018ab003541f4
estebancabreraa <esteban.cabrera.a@gmail.com>
2022-04-04 09:42:59 -0600

    test ignore

--- scripts/api.py
+++ scripts/api.py
@@ -13,3 +13,15 @@ def user_auth(email, password):
     except Exception as e:
         print("Ocurrió un error al conectar a SQL Server: ", e)
         return False
+
+print(user_auth('programadorit@laats.net', '7070'))
+
+def get_flights(date):
+    try: 
+        with pyodbc.connect('DRIVER='+DRIVER+';SERVER=tcp:'+SERVER+';PORT=1433;DATABASE='+DATABASE+';UID='+USERNAME+';PWD='+ PASSWORD) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT usuario, pin FROM usuarios WHERE usuario ='" + email + "' And pin ='" + password + "';")
+                result = cursor.fetchall()
+    except Exception as e:
+        print("Ocurrió un error al conectar a SQL Server: ", e)
+        return False
