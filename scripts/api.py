3a731c21c1aba70dea4978f34e8a155d076d65d5
estebancabreraa <esteban.cabrera.a@gmail.com>
2022-04-05 15:02:10 -0600

    dark mode set && reset pin query done && pin inputs maxlenght set to 4

--- scripts/api.py
+++ scripts/api.py
@@ -13,12 +13,29 @@ def user_auth(email, password):
     except Exception as e:
         print("Ocurrió un error al conectar a SQL Server: ", e)
         return False
+
+def pin_reset(email, last_pin, last_pin_conf, new_pin):
+    if last_pin == last_pin_conf:
+        if user_auth(email, last_pin):
+            try: 
+                with pyodbc.connect('DRIVER='+DRIVER+';SERVER=tcp:'+SERVER+';PORT='+PORT+';DATABASE='+DATABASE+';UID='+USERNAME+';PWD='+ PASSWORD) as conn:
+                    with conn.cursor() as cursor:
+                        cursor.execute(get_pin_reset_query(email, new_pin))
+                        result = cursor.fetchall()
+                        return result
+            except Exception as e:
+                print("Ocurrió un error al conectar a SQL Server: ", e)
+                return False
+        else:
+            return False
+    else:
+        return False
         
-def get_flights(day, month, operator, departamento):
+def get_flights(day, month, operator, departament):
     try: 
         with pyodbc.connect('DRIVER='+DRIVER+';SERVER=tcp:'+SERVER+';PORT='+PORT+';DATABASE='+DATABASE+';UID='+USERNAME+';PWD='+ PASSWORD) as conn:
             with conn.cursor() as cursor:
-                cursor.execute(get_flights_query(day, month, operator, departamento))
+                cursor.execute(get_flights_query(day, month, operator, departament))
                 result = cursor.fetchall()
                 return result
     except Exception as e:
