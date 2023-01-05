37679de4d1c2ea6e93c74120aaf7ff827cd210a5
estebancabreraa <esteban.cabrera.a@gmail.com>
2023-01-04 23:33:52 -0600

    .env documentation

--- scripts/env.py
+++ scripts/env.py
@@ -1,5 +1,25 @@
+'''
+    @Author: Esteban Cabrera.
+    @Version: 1.0.0.
+    @See: app.py, query_builder.py.
+
+    Este script contiene métodos creados para generar los queries a ejecutar en la base de datos
+    para obtener la información deseada a base de parámetros establecidos.
+'''
+
 import scripts.env
 
+'''
+    Credenciales de la base de datos 'aatl' en Azure SQL Server:
+
+    SERVER -> Referencia a la DNS del servidor de bases de datos mysqllaats.
+    DATABASE -> Referencia a la base de datos 'aatl'.
+    USERNAME -> Referencia al nombre de usuario del servidor.
+    PASSWORD -> Referencia a la clave de usuario del servidor.
+    DRIVER -> Referencia al nombre del DRIVER de base de datos requerido para la conexion y transacciones
+              a realizar.
+    PORT -> Referencia al puerto establecido en el servidor de bases de datos para permitir la conexion.
+'''
 SERVER = 'mysqllaats.database.windows.net'
 DATABASE = 'aatl'
 USERNAME = 'AdminLaats'
@@ -7,16 +27,32 @@ PASSWORD = 'Fn_1_5Laats'
 DRIVER = '{ODBC Driver 17 for SQL Server}' 
 PORT = '1433'
 
+'''
+    Variables de ambiente para la gestion de sesion del usuario.
+
+    USER_ID (int) -> Almacena temporalmente el identificador del usuario autenticado.
+    DEPARTMENT (string) -> Almacena temporalmente el nombre del departamento al que pertenece el usuario.
+    DEPARTMENT_COLOR (string) -> Almacena el color definido para el departamento al que pertenece 
+    el usuario.
+    LOGGED_IN (bool) -> Almacena una bandera True o False para determinar si un usuario esta autenticado.
+'''
 USER_ID = ''
 DEPARTMENT = ''
-
 DEPARTMENT_COLOR = ''
-
 LOGGED_IN = False
 
+'''
+    Variable de ambiente que contiene los nombres de los agentes del departamento de PXS.
+
+    Ver en app.py.
+'''
 PXS_PERSONEL_CONFIG = ['CREW CHIEF', 'AGENTES DE RAMPA', 'RAYOS X', 'MOSTRADOR', 'OPERADORES', 'FAJAS ENTRANDO']
 
 def set_department_color():
+    '''
+        Este metodo se utiliza para asignarle el valor del color determinado para el departamento al que
+        pertenece el usuario atenticado a la variable de ambiente DEPARTMENT_COLOR
+    '''
     hex_color = '#ffffff'
     if DEPARTMENT == 'PXS':
         hex_color = '#8F99F7'
